7f2d0bb95bd62171e9a0240fc67ea0a61ec2cda8
Batu <BatuhanYasar006@gmail.com>
2023-11-11 09:22:11 +0300

    IDistrubuted Cache işlemleri - byte ve string üzerinde cacheleme işlemleri yapıldı.-

---
+++ IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
@@ -0,0 +1,107 @@
+﻿using IDistributedCacheRedisApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace IDistributedCacheRedisApp.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private IDistributedCache _distributedCache;
+
+
+        public ProductsController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        /*         
+         *       ---  Burada string serilize yaptık. ---
+         *         
+                public async Task<IActionResult> Index()
+                {
+                    DistributedCacheEntryOptions cacheEntryOptions = new DistributedCacheEntryOptions();
+
+                    cacheEntryOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(10);
+
+                    // 1. Burada basit cache işlemi için bir keye değer atadık.
+                    // _distributedCache.SetString("name","Batuhan",cacheEntryOptions);
+
+                    // await _distributedCache.SetStringAsync("surname", "YASAR");
+
+
+
+                    Product product = new Product { Id = 1, Name = "Kalem", Price = 200 }; 
+                    // a.1. JSON Serilize işlemi gerçekletiriyoruz.
+                    string jsonProduct =  JsonConvert.SerializeObject(product);
+
+                    await _distributedCache.SetStringAsync("product:1", jsonProduct,cacheEntryOptions);
+
+
+                    return View();
+                }
+
+                public IActionResult Show()
+                {    // 2. Burada cache işlemin alıyoruz ve Viewbag ile yansıtıyor.
+                     // string name = _distributedCache.GetString("name");
+
+
+                    // ViewBag.name = name;
+
+                    string jsonProduct = _distributedCache.GetString("product:1");
+                    // a.2. JSON Deserilize işlemi gerçekletiriyoruz.
+                    Product p = JsonConvert.DeserializeObject<Product>(jsonProduct);
+
+                    ViewBag.product = p;
+
+                    return View();
+                }
+        */
+
+        //               --- Byte ile Serilize Yapıyoruz ---
+
+        public async Task<IActionResult> Index()
+        {
+            DistributedCacheEntryOptions cacheEntryOptions = new DistributedCacheEntryOptions();
+
+            cacheEntryOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(10);
+
+
+            Product product = new Product { Id = 1, Name = "Kalem", Price = 200 };
+            // a.1. JSON Binary Serilize işlemi gerçekletiriyoruz.
+            string jsonProduct = JsonConvert.SerializeObject(product);
+
+            Byte[] byteProduct = Encoding.UTF8.GetBytes(jsonProduct);
+
+            _distributedCache.Set("product:1", byteProduct);
+
+
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            Byte[] byteProduct = _distributedCache.Get("product:1");
+
+            string jsonProduct = Encoding.UTF8.GetString(byteProduct);
+
+            // a.2. JSON Deserilize işlemi gerçekletiriyoruz.
+            Product p = JsonConvert.DeserializeObject<Product>(jsonProduct);
+
+            ViewBag.product = p;
+
+            return View();
+        }
+
+        public IActionResult Remove()
+        {
+            // 3. Burada basit cache işlemini siliyor.
+            _distributedCache.Remove("name");
+
+            return View();
+
+        }
+
+    }
+}
